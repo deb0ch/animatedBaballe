acaad861c076725151669e420dbe30af4c7eba0e
deb0ch <thomas.de.beauchene@gmail.com>
2018-03-27 12:02:22 +0200

    animate a baballe on the screen

--- App.js
+++ App.js
@@ -1,58 +1,80 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component }    from 'react';
+import { Animated,
+         Dimensions,
+         Easing,
+         StyleSheet,
+         Text,
+         TouchableOpacity,
+         View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  constructor () {
+    super();
+    this.animatedBaballe = new Animated.Value(0);
+  }
+
+  animate () {
+    this.animatedBaballe.setValue(0);
+    const heightAnimation = Animated.timing(
+      this.animatedBaballe,
+      {
+        toValue: 1,
+        duration: 5000,
+        easing: Easing.bounce,
+      },
+    );
+    Animated.loop(heightAnimation).start();
+  }
+
   render() {
+    const animatedMargin = this.animatedBaballe.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [0, Dimensions.get('window').height, 0]
+    });
+    const animatedColor = this.animatedBaballe.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: ["#123", "#f109bc", "#123"]
+    });
+    const animatedScale = this.animatedBaballe.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [1, 2, 1]
+    });
+    const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <AnimatedTouchableOpacity onPress={this.animate.bind(this)}
+                                  style={[styles.baballe, {
+                                    backgroundColor: animatedColor,
+                                    marginBottom: animatedMargin,
+                                    transform: [{scale: animatedScale}],
+                                  }]}
+        >
+        </AnimatedTouchableOpacity>
       </View>
     );
   }
 }
 
+
+const baballeSize = 44;
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'flex-end',
+    alignSelf: 'center',
+    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  baballe: {
+    marginLeft: (Dimensions.get('window').width - baballeSize) / 2,
+    marginRight: (Dimensions.get('window').width - baballeSize) / 2,
+    width: baballeSize,
+    height: baballeSize,
+    borderRadius: baballeSize / 2,
+  }
 });
