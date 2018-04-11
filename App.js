15e1add54d3af79bc68e1661c6821fbd6b5e4aa2
deb0ch <thomas.de.beauchene@gmail.com>
2018-04-11 17:12:35 +0200

    animate the ball like a coin thrown on a plane with friction

--- App.js
+++ App.js
@@ -10,11 +10,17 @@ import { Animated,
          View } from 'react-native';
 
 
+const baballeSize = 44;
+const baballeFriction = 0.1
+const baballeMass = 10
+
+
 export default class App extends Component {
   constructor () {
     super();
-    this.animatedBaballe = new Animated.Value(0);
-    this.animatedBaballeSize = new Animated.Value(baballeSize);
+    const initialHeight = (Dimensions.get('window').height - baballeSize) / 2.0;
+    this.animatedBaballePoseY = new Animated.Value(initialHeight);
+    this.animatedBaballeOnTouch = new Animated.Value(1.0);
   }
 
   componentWillMount () {
@@ -26,70 +32,62 @@ export default class App extends Component {
       onPanResponderTerminationRequest: (evt, gestureState) => true,
       onShouldBlockNativeResponder: (evt, gestureState) => true,
       onPanResponderGrant: (evt, gestureState) => { this.animateOnTouch(); },
-      onPanResponderMove: (evt, gestureState) => {},
+      onPanResponderMove: (evt, gestureState) => {
+        this.animatedBaballePoseY.setValue(gestureState.moveY);
+      },
       onPanResponderRelease: (evt, gestureState) => {
-        if (gestureState.dy > 15)
-          this.animateBounce();
+          this.animateThrow(gestureState.moveY, gestureState.vy);
       },
       onPanResponderTerminate: (evt, gestureState) => {},
     });
   }
 
-  animateBounce() {
-    this.animatedBaballe.setValue(0);
-    const heightAnimation = Animated.timing(
-      this.animatedBaballe,
-      {
-        toValue: 1,
-        duration: 5000,
-        easing: Easing.bounce,
+  getBaballeTravelTime(v0)
+  {
+    return baballeMass * ((Math.abs(v0) + Math.sqrt((v0 ** 2) + 1))
+                          / baballeFriction);
+  }
+
+  getBaballeTravelDistance(v0)
+  {
+    const sign = v0 > 0 ? 1 : -1;
+    return sign * ((v0 ** 2) * baballeMass / (2 * baballeFriction));
+  }
+
+  animateThrow(currentY, speedY) {
+    console.log(" ---------------------------- ");
+    console.log("v0 = ", speedY);
+    console.log("dist = ", this.getBaballeTravelDistance(speedY));
+    console.log("time = ", this.getBaballeTravelTime(speedY));
+    console.log("dist / time = ", (this.getBaballeTravelDistance(speedY)
+                                   / this.getBaballeTravelTime(speedY)));
+    console.log(" ---------------------------- ");
+    Animated.timing(
+      this.animatedBaballePoseY, {
+        toValue: currentY + this.getBaballeTravelDistance(speedY),
+        duration: this.getBaballeTravelTime(speedY),
+        easing: Easing.easeOutQuad,
       },
-    );
-    // Animated.loop(heightAnimation).start();
-    heightAnimation.start();
+    ).start();
   }
 
   animateOnTouch() {
-    this.animatedBaballeSize.setValue(baballeSize * 0.9);
+    this.animatedBaballeOnTouch.setValue(1.0);
     Animated.spring(
-      this.animatedBaballeSize,
-      {
-        toValue: baballeSize,
+      this.animatedBaballeOnTouch, {
+        toValue: 1.0,
         friction: 1.5,
-        velocity: 10,
+        velocity: -1,
       },
     ).start();
   }
 
   render() {
-    const animatedMargin = this.animatedBaballe.interpolate({
-      inputRange: [0, 0.5, 1],
-      outputRange: [0, Dimensions.get('window').height, 0]
-    });
-    const animatedColor = this.animatedBaballe.interpolate({
-      inputRange: [0, 0.5, 1],
-      outputRange: ["#ea296a", "#ffeb3b", "#ea296a"]
-    });
-    const animatedScale = this.animatedBaballe.interpolate({
-      inputRange: [0, 0.5, 1],
-      outputRange: [1, 2, 1]
-    });
-    const animatedBorderRadius = Animated.divide(this.animatedBaballeSize, 2.0);
-    const animatedSideMargin = Animated.divide(
-      Animated.add(Dimensions.get('window').width,
-                   Animated.multiply(this.animatedBaballeSize, -1)),
-      2.0);
     return (
       <View style={styles.container}>
         <Animated.View style={[styles.baballe, {
-                          backgroundColor: animatedColor,
-                          marginBottom: animatedMargin,
-                          transform: [{scaleY: animatedScale}],
-                          width: this.animatedBaballeSize,
-                          height: this.animatedBaballeSize,
-                          borderRadius: animatedBorderRadius,
-                          marginLeft: animatedSideMargin,
-                          marginRight: animatedSideMargin,
+                         transform: [{scale: this.animatedBaballeOnTouch}],
+                         top: this.animatedBaballePoseY,
                        }]}
                        {...this.panResponder.panHandlers}
         >
@@ -100,18 +98,23 @@ export default class App extends Component {
 }
 
 
-const baballeSize = 44;
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#4b5cba',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
     alignItems: 'flex-end',
     alignSelf: 'center',
     flexDirection: 'row',
     justifyContent: 'center',
   },
   baballe: {
+    backgroundColor: "#ea296a",
+    position: 'absolute',
+    left: (Dimensions.get('window').width - baballeSize) / 2.0,
+    borderRadius: baballeSize / 2.0,
+    width: baballeSize,
+    height: baballeSize,
   }
 });
